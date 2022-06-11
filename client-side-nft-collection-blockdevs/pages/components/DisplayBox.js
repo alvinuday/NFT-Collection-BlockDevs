d83a805d2bfdce59f4718e14ba381e7a64a5f731
Alvin Uday <udayalvin@gmail.com>
2022-06-11 17:30:43 +0530

    fix: class to className

--- client-side-nft-collection-blockdevs/pages/components/DisplayBox.js
+++ client-side-nft-collection-blockdevs/pages/components/DisplayBox.js
@@ -6,8 +6,8 @@ export default function textBox(props) {
   }
   return (
     <div>
-      <div class="p-6 max-w-sm bg-white rounded-xl shadow-lg flex items-center space-x-4 hover:shadow-md">
-        <div class="shrink-0">
+      <div className="p-6 max-w-sm bg-white rounded-xl shadow-lg flex items-center space-x-4 hover:shadow-md">
+        <div className="shrink-0">
           <Image
             layout="intrinsic"
             src={imageurl}
@@ -17,7 +17,7 @@ export default function textBox(props) {
           ></Image>
         </div>
         <div>
-          <div class="text-lg font-medium text-black">
+          <div className="text-lg font-medium text-black">
             Check your NFT out here
           </div>
           <a
@@ -26,7 +26,7 @@ export default function textBox(props) {
             rel="noopener noreferrer"
             href={`https://testnets.opensea.io/assets/rinkeby/0x968f65eb30d01439a969074252ce6c3b423f9362/${props.link}`}
           >
-            <p class="text-slate-500 hover:text-black cursor-pointer">
+            <p className="text-slate-500 hover:text-black cursor-pointer">
               Block Dev#{props.link}
             </p>
           </a>
