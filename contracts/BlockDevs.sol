ceeff08e71ba4c1b54806d82ea07d2078498b6e2
Alvin Uday <udayalvin@gmail.com>
2022-06-11 16:13:32 +0530

    change: max number of NFTs that can be minted

--- contracts/BlockDevs.sol
+++ contracts/BlockDevs.sol
@@ -19,7 +19,7 @@
       bool public _paused;
 
       // max number of BlockDevs
-      uint256 public maxTokenIds = 20;
+      uint256 public maxTokenIds = 10;
 
       // total number of tokenIds minted
       uint256 public tokenIds;
